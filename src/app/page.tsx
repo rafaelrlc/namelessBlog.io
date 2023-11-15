56140484e9db243d1dbf71621cfeeb3432ecd833
Rafael Ribeiro <105018495+rafaelrlc@users.noreply.github.com>
2023-11-15 16:31:02 -0300

    feat : homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,23 @@ import MenuOptions from "./components/MenuOptions";
 import { getPostMetadata } from "./lib/functions";
 import Header from "./components/Header";
 
-export default function HomePage() {
+export default async function HomePage() {
   const postMetadata = getPostMetadata();
+
+  const response = await fetch(`http://localhost:8080/api/post`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      "Access-Control-Allow-Headers":
+        "Origin, X-Requested-With, Content-Type, Accept",
+    },
+  });
+
+  const data = await response.json();
+  console.log(data);
+
   const postPreviews = postMetadata.map((slug) => {
     return <Post slug={slug} key={slug} />;
   });
