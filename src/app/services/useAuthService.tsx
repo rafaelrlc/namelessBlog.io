f51697f77d8acb9922e711720cf3254dc53b6eb0
Rafael Ribeiro <105018495+rafaelrlc@users.noreply.github.com>
2023-11-15 16:30:33 -0300

    refactor : remove next api code

--- src/app/services/useAuthService.tsx
+++ src/app/services/useAuthService.tsx
@@ -9,54 +9,13 @@ export const useAuthService = () => {
   const { toast } = useToast();
 
   const login = async (values: z.infer<typeof LoginSchema>) => {
-    const response = await signIn("credentials", {
-      email: values.email,
-      password: values.password,
-      redirect: false,
-    });
-
-    console.log(response);
-
-    if (response?.error) {
-      toast({
-        title: "Credenciais inválidas",
-        description: "Ocorreu um erro ao tentar fazer login.",
-        variant: "destructive",
-      });
-    } else {
-      toast({
-        title: "Login realizado com sucesso",
-        description: "Você será redirecionado em breve.",
-        variant: "accept",
-      });
-      push("/");
-    }
+    console.log(values);
+    push("/");
   };
 
   const register = async (values: z.infer<typeof SignUpSchema>) => {
-    const response = await fetch("api/user", {
-      method: "POST",
-      body: JSON.stringify({
-        username: values.username,
-        email: values.email,
-        password: values.password,
-        name: values.name,
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    if (response.ok) {
-      push("/sign-in");
-      console.log(response);
-    } else {
-      toast({
-        title: "Erro ao criar conta",
-        description: "Ocorreu um erro ao tentar criar sua conta.",
-        variant: "destructive",
-      });
-    }
+    console.log(values);
+    push("/sign-in");
   };
 
   return { login, register };
