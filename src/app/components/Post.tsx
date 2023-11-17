790a015bdf3a59d2cf0d93fc38b44f55cbe0cdae
Rafael Ribeiro <105018495+rafaelrlc@users.noreply.github.com>
2023-11-17 14:23:06 -0300

    refactor : post page

--- src/app/components/Post.tsx
+++ src/app/components/Post.tsx
@@ -2,26 +2,26 @@ import Link from "next/link";
 import React from "react";
 
 interface PostProps {
-  slug: string;
+  title: string;
+  id: number;
 }
 
-const Post = ({ slug }: PostProps) => {
+const Post = ({ id, title }: PostProps) => {
   return (
     <div className="border rounded-md p-4 flex flex-col gap-4">
       <div className="flex flex-col gap-1">
         <p className="text-gray-400 text-sm">2020-12-27</p>
         <Link
-          href={`/post/${slug}`}
+          href={`/post/${id}`}
           className="text-indigo-700 hover:text-indigo-800 hover:underline hover:font-semibold"
         >
-          {slug}
+          {title}
         </Link>
       </div>
 
       <p className="line-clamp-3 text-gray-600">
         Lorem Ipsum is simply dummy text of the printing and typesetting Lorem
-        Ipsum is simply dummy text of the printing and typesetting Lorem Ipsum
-        is simply dummy text of the printing and typesetting Lorem Ipsum is
+        Ipsum is simply dummy text of the
       </p>
     </div>
   );
