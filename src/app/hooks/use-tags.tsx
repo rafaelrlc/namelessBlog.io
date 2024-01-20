f8c28087f6a5d1eab70b6e36e91e68aacf70f931
Rafael Ribeiro <105018495+rafaelrlc@users.noreply.github.com>
2024-01-20 20:25:56 -0300

    feat : use-tags and use-getData hooks

---
+++ src/app/hooks/use-tags.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Badge } from "../components/ui/badge";
+
+interface BadgeData {
+  id: number;
+  badge: React.ReactElement;
+}
+
+const badgesList: BadgeData[] = [
+  {
+    id: 1,
+    badge: <Badge variant="blue">Anime 🍥</Badge>,
+  },
+  {
+    id: 2,
+    badge: <Badge variant="green">Art 🎨</Badge>,
+  },
+  {
+    id: 3,
+    badge: <Badge variant="yellow">Inspiration ✨</Badge>,
+  },
+];
+
+const useBadgeById = (id: number): React.ReactElement | null => {
+  const selectedBadge = badgesList.find((badge) => badge.id === id);
+  return selectedBadge ? selectedBadge.badge : null;
+};
+
+export default useBadgeById;
