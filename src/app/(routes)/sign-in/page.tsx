6b020726cdcca05c24d2b9ceb2eb47bb5a7c7fa2
Rafael Ribeiro <105018495+rafaelrlc@users.noreply.github.com>
2024-01-20 20:28:05 -0300

    fix sign-in page css

--- src/app/(routes)/sign-in/page.tsx
+++ src/app/(routes)/sign-in/page.tsx
@@ -3,7 +3,7 @@ import { Label } from "@/app/components/ui/label";
 
 export default function LoginForm() {
   return (
-    <div className="h-screen flex flex-col items-center justify-center">
+    <div className="flex flex-col mx-auto items-center justify-center max-w-xl">
       <Label className="text-2xl font-bold mb-4">Login</Label>
       <SignInForm />
     </div>
