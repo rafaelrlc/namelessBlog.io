ca4de2c19587b89e82703b505572c6fec6787e2e
Rafael Ribeiro <105018495+rafaelrlc@users.noreply.github.com>
2024-01-20 20:28:21 -0300

    update

--- src/app/components/FooterComponent.tsx
+++ src/app/components/FooterComponent.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const FooterComponent = () => {
   return (
     <footer className="max-w-4xl py-4">
-      <div className="border-t border-slate-400 mt-6 py-6 text-center text-slate-400">
+      <div className="border-t border-slate-400 mt-6 py-6 text-center text-slate-500">
         <p>© 2023 NameLess, Inc</p>
       </div>
     </footer>
--- src/app/components/MenuOptions.tsx
+++ src/app/components/MenuOptions.tsx
@@ -1,18 +1,16 @@
 import React from "react";
 import Link from "next/link";
-import signOut from "next-auth/react";
-import { Button } from "./ui/button";
 
 const MenuOptions = () => {
   return (
     <div className="grid grid-cols-2 gap-4">
       <Link href={"/profile"}>
-        <p className="bg-primary hover:bg-primary/90 text-center text-white font-bold mb-10 py-2 px-4 rounded">
+        <p className="bg-primary hover:bg-primary/90 text-center text-white font-medium mb-10 py-2 px-4 rounded-lg">
           Seu Perfil
         </p>
       </Link>
       <Link href={"/post/new"}>
-        <p className="bg-primary hover:bg-primary/90 text-center text-white font-bold mb-10 py-2 px-4 rounded">
+        <p className="bg-primary hover:bg-primary/90 text-center text-white font-medium mb-10 py-2 px-4 rounded-lg">
           Novo Post
         </p>
       </Link>
